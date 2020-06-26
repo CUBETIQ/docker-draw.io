bc1684db20a174af1c789d379489745027f786cd
Sambo Chea <sombochea@cubetiqs.com>
2020-06-26 10:54:46 +0700

    Updated and add build alpine docker image

---
+++ buildAlpine.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+REGISTRY=registry.cubetiqs.com
+CONTAINER=onedraw
+TAG=0.1-alpine
+ROOT_CONTAINER=$REGISTRY/$CONTAINER:$TAG
+
+echo "Building image..."
+docker build -f alpine/Dockerfile . -t $ROOT_CONTAINER
+
+echo "Pushing image to $ROOT_CONTAINER"
+docker push $ROOT_CONTAINER
